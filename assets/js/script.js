2e223a0e74928fc76aab8b84b69cc64d7d214b62
CMcCown <connermccown94@gmail.com>
2022-02-12 13:07:37 -0700

    created js file and added qestion array

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+// create array and pass the number, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What is 2 + 2?",
+        answer: "4",
+        options: [
+            "3",
+            "5",
+            "6",
+            "4"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is 2 x 5?",
+        answer: "10",
+        options: [
+            "8",
+            "10",
+            "15",
+            "5"
+        ]
+    },
+];
