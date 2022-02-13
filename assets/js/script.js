d39fe8dffdc8546eb504eb93648d757609bd19b0
DawgGuy <connermccown94@gmail.com>
2022-02-13 12:50:02 -0700

    added option to allow entry of initals for highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -202,18 +202,24 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
+    const highScore = result_box.querySelector(".score_text");
     if (userScore > 3){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>And congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        let highTag = '<form> Save your highscore of ' + userScore + '<br> Enter your Initals: <br> <input type="text" name="initials"></form>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+        highScore.innerHTML = highTag;
+        
     }
     else if(userScore > 1){ // if user scored more than 1
         let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
+        
     }
     else{ // if user scored less than 1
         let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
+        
     }
 }
 
